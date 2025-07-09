59c12d93703496d8cae9f8c52a1b42abfa471d99
Benjamin Lerer <b.lerer@gmail.com>
2025-07-09 18:11:25 +0200

    Add tests for parametrize

--- src/query/optimizer.rs
+++ src/query/optimizer.rs
@@ -64,3 +64,101 @@ impl Optimizer {
 pub trait NormalisationRule {
     fn apply(&self, plan: Arc<LogicalPlan>) -> Arc<LogicalPlan>;
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::query::logical::expr_fn::{and, eq, field, field_filters};
+    use crate::query::logical::logical_plan::LogicalPlanBuilder;
+    use crate::query::logical::{BsonValue, ComparisonOperator, Expr};
+
+    #[test]
+    fn test_parametrize_simple_filter() {
+        let optimizer = Optimizer::new(vec![]);
+        let plan = LogicalPlanBuilder::scan("db", "collection")
+            .filter(field_filters(field(["a"]), vec![eq(10)]))
+            .build();
+
+        let (parametrized_plan, params) = optimizer.parametrize(plan);
+
+        // Check params
+        assert_eq!(params.len(), 1);
+        assert_eq!(params.get(0), Some(&BsonValue::from(10)));
+
+        // Check plan
+        let expected_condition = Arc::new(Expr::FieldFilters {
+            field: field(["a"]),
+            filters: vec![Arc::new(Expr::Comparison {
+                operator: ComparisonOperator::Eq,
+                value: Arc::new(Expr::Placeholder(0)),
+            })],
+        });
+        let expected_plan = LogicalPlanBuilder::scan("db", "collection")
+            .filter(expected_condition)
+            .build();
+
+        assert_eq!(parametrized_plan, expected_plan);
+    }
+
+    #[test]
+    fn test_parametrize_complex_filter() {
+        let optimizer = Optimizer::new(vec![]);
+        let plan = LogicalPlanBuilder::scan("db", "collection")
+            .filter(and(vec![
+                field_filters(field(["a"]), vec![eq(10)]),
+                field_filters(field(["b"]), vec![eq("hello")]),
+            ]))
+            .build();
+
+        let (parametrized_plan, params) = optimizer.parametrize(plan);
+
+        // Check params
+        assert_eq!(params.len(), 2);
+        assert_eq!(params.get(0), Some(&BsonValue::from(10)));
+        assert_eq!(params.get(1), Some(&BsonValue::from("hello")));
+
+        // Check plan
+        let expected_condition = and(vec![
+            Arc::new(Expr::FieldFilters {
+                field: field(["a"]),
+                filters: vec![Arc::new(Expr::Comparison {
+                    operator: ComparisonOperator::Eq,
+                    value: Arc::new(Expr::Placeholder(0)),
+                })],
+            }),
+            Arc::new(Expr::FieldFilters {
+                field: field(["b"]),
+                filters: vec![Arc::new(Expr::Comparison {
+                    operator: ComparisonOperator::Eq,
+                    value: Arc::new(Expr::Placeholder(1)),
+                })],
+            }),
+        ]);
+
+        let expected_plan = LogicalPlanBuilder::scan("db", "collection")
+            .filter(expected_condition)
+            .build();
+
+        assert_eq!(parametrized_plan, expected_plan);
+    }
+
+    #[test]
+    fn test_parametrize_no_literals() {
+        let optimizer = Optimizer::new(vec![]);
+        let plan = LogicalPlanBuilder::scan("db", "collection")
+            .filter(field_filters(
+                field(["a"]),
+                vec![Arc::new(Expr::Exists(true))],
+            ))
+            .build();
+        let plan_clone = plan.clone();
+
+        let (parametrized_plan, params) = optimizer.parametrize(plan);
+
+        // Check params
+        assert_eq!(params.len(), 0);
+
+        // Check plan
+        assert_eq!(parametrized_plan, plan_clone);
+    }
+}
