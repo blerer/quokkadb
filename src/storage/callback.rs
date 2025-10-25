97dd2ae630eb30754438d91b6f3213ed1840677e
Benjamin Lerer <b.lerer@gmail.com>
2025-10-25 18:06:12 +0200

    Fix StorageEngine.wait_for_pending_flushes

--- src/storage/callback.rs
+++ src/storage/callback.rs
@@ -2,8 +2,6 @@ use crate::obs::logger::{LogLevel, LoggerAndTracer};
 use std::io::{Error, ErrorKind, Result};
 use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
 use std::sync::{Arc, Mutex};
-use std::thread;
-use std::thread::JoinHandle;
 use crate::error;
 
 pub trait Callback<T>: Sync + Send {
@@ -11,75 +9,72 @@ pub trait Callback<T>: Sync + Send {
 }
 
 pub struct AsyncCallback<T> {
-    sender: SyncSender<T>,
-    thread: Option<JoinHandle<()>>,
+    logger: Arc<dyn LoggerAndTracer>,
+    fun: Box<dyn Fn(T) -> Result<()> + Send + Sync>,
 }
 
-impl<T: Send + Sync> AsyncCallback<T> {
+impl<T: Send + 'static> AsyncCallback<T> {
     pub fn new<F>(logger: Arc<dyn LoggerAndTracer>, f: F) -> Self
     where
-        F: Fn(T) -> Result<()> + Send + 'static,
-        T: Send + Sync + 'static,
+        F: Fn(T) -> Result<()> + Send + Sync + 'static,
     {
-        let (sender, receiver): (SyncSender<T>, Receiver<T>) = sync_channel(32);
-
-        let thread = {
-            thread::spawn(move || {
-                while let Ok(result) = receiver.recv() {
-                    let result = f(result);
-                    if let Err(err) = result {
-                        error!(logger, "AsyncCallback function returned an error: {}", err);
-                    }
-                }
-            })
-        };
-
         Self {
-            sender,
-            thread: Some(thread),
+            logger,
+            fun: Box::new(f),
         }
     }
 
     pub fn call(&self, value: T) {
-        self.sender.send(value).unwrap();
+        if let Err(err) = (self.fun)(value) {
+            error!(self.logger, "AsyncCallback function returned an error: {}", err);
+        }
     }
 }
 
+use std::marker::PhantomData;
+
 pub struct BlockingCallback<T, F> {
-    sender: SyncSender<T>,
-    receiver: Arc<Mutex<Option<Receiver<T>>>>,
+    sender: SyncSender<Result<()>>,
+    receiver: Arc<Mutex<Option<Receiver<Result<()>>>>>,
     fun: F,
+    _phantom: PhantomData<T>,
 }
 
 impl<T: Send + Sync, F: Fn(T) -> Result<()> + Send + Sync + 'static> Callback<T>
     for BlockingCallback<T, F>
 {
     fn call(&self, value: T) {
-        self.sender.send(value).unwrap();
+        let result = (self.fun)(value);
+        self.sender.send(result).unwrap();
     }
 }
 
-impl<T: Send + Sync, F: Fn(T) -> Result<()> + Send + 'static> BlockingCallback<T, F> {
+impl<T, F> BlockingCallback<T, F>
+where
+    F: Fn(T) -> Result<()> + Send + Sync + 'static,
+{
     pub fn new(f: F) -> Self {
-        let (sender, receiver): (SyncSender<T>, Receiver<T>) = sync_channel(1);
+        let (sender, receiver): (SyncSender<Result<()>>, Receiver<Result<()>>) = sync_channel(1);
 
         Self {
             sender,
             receiver: Arc::new(Mutex::new(Some(receiver))),
             fun: f,
+            _phantom: PhantomData,
         }
     }
 
     pub fn await_blocking(&self) -> Result<()> {
         if let Some(receiver) = self.receiver.lock().unwrap().take() {
             match receiver.recv() {
-                Ok(result) => return (self.fun)(result),
-                Err(error) => return Err(Error::new(ErrorKind::Interrupted, error)),
+                Ok(result) => result,
+                Err(error) => Err(Error::new(ErrorKind::Interrupted, error)),
             }
+        } else {
+            Err(Error::new(
+                ErrorKind::Other,
+                "Unsupported await_blocking call to an already exhausted callback",
+            ))
         }
-        Err(Error::new(
-            ErrorKind::Other,
-            "Unsupported await_blocking call to an already exhausted callback",
-        ))
     }
 }
